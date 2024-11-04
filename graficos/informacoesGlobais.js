fcd1df684e9830bebea443cc6a6dae8a37db6841
Kássia Belém Anacleto <114186521+kahbelem@users.noreply.github.com>
2024-11-04 10:38:37 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,12 +6,12 @@ async function vizualizarinformacoesGlobais() {
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
-    const minutos = (dados.tempo_medio - horas) * 100
+    const minutos =Math.round ((dados.tempo_medio - horas) * 100)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
 
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas </span> e ${minutos} minutos conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas </span> e <span>${minutos} minutos</span> conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
